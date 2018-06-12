d60c9be9f50f75ebcb30ce2d6290087dd4484407
jcjcarlos <jjc_carlos@live.com>
2018-06-12 11:01:28 -0300

    Removendo testes

--- temp_client.py
+++ temp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-ip = "127.0.0.1"
-porta = 4321
+ip = ''
+porta = 9876
 
 cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 cliente.connect((ip,porta))
@@ -10,5 +10,6 @@ mensagem = cliente.recv(1024)
 print (mensagem.decode('ascii'))
 opcao = input('Digite uma opcao')
 while (opcao != '-1'):
+    print cliente.recv(1024)"""
     cliente.send(opcao.encode('ascii'))
-    opcao = raw_input(">")
+    opcao = raw_input(">")"""
