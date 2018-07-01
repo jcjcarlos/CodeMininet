c5dbd872d53d616738a9a858eae8bcb98a5e8689
carlosjjc <jorge.goncalves@ifba.edu.br>
2018-07-01 07:20:53 -0300

    Alterando cliente

--- RemoteLogger.py
+++ RemoteLogger.py
@@ -2,8 +2,12 @@
 from mininet.log import lg,setLogLevel
 import cPickle
 import logging
-
-class SocketHandlerMininet(logging.SocketHandler):
+from logging import handlers
+#A biblioteca SocketHandler utiliza cPickle ao inves de pickle
+class SocketHandlerMininet(handlers.SocketHandler):
+    def __init__(self,host='',port=handlers.DEFAULT_TCP_LOGGING_PORT):
+        handlers.SocketHandler.__init__(self,host,port)
+        
     """
     Remocao da quebra de linha dupla na serializacao da mensagem,
     O metodo makePickle foi redefinido para enviar somente a string
@@ -14,14 +18,14 @@ class SocketHandlerMininet(logging.SocketHandler):
         try:
             msg = record.getMessage() # Ou format(record)
             fmt = '%s'
-            sock.send(cPickle.dumps(fmt % msg)) # Codificacao unicode no topo
+            self.sock.send(cPickle.dumps(fmt % msg)) # Codificacao unicode no topo
             return cPickle.dumps(msg)
         except Exception:
             self.handleError( record )
             return cPickle.dumps('*** Error ')
 
 def main():
-    socket_handler = SocketHandlerMininet('', DEFAULT_TCP_LOGGING_PORT)
+    socket_handler = SocketHandlerMininet('', handlers.DEFAULT_TCP_LOGGING_PORT)
     socket_handler.setLevel(logging.INFO)
     lg.addHandler(socket_handler)
     setLogLevel('info')
--- client.py
+++ client.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from mininet.topo import Topo, SingleSwitchTopo, MinimalTopo
-from mininet.topo import SingleSwitchReversedTopo, LinearTopo
+from mininet.topo import *
 from mininet.net import Mininet
 from mininet.log import setLogLevel
 from mininet.cli import CLI
 from socket import socket,AF_INET,SOCK_STREAM
-import Pyro4
-from os import system
-from sys import exit
+from logging.handlers import DEFAULT_TCP_LOGGING_PORT
+import SocketServer
+import threading
+
 """
 Pyro4.config.SERIALIZERS_ACCEPTED = set(['json', 'marshal', 'serpent','pickle'])
 Pyro4.config.SERIALIZER = 'json'
@@ -17,19 +17,70 @@ Pyro4.config.SERIALIZER = 'json'
            
 class RemoteNetwork(object):
     def __init__(self):
-        self.hosts = {}
-        self.switches = {}
-        self.controllers = {}
-        self.network = None
         self.server = socket(AF_INET,SOCK_STREAM)
-        self.server.connect(('',9876))
+        self.client = None
+    
+    def connection_client(self, host='localhost'):
+        isValid = self.validate_ip(host):
+        if isValid:
+            self.server.bind(host,DEFAULT_TCP_LOGGING_PORT)
+            self.server.listen(1)
+            new_client, addr = self.server.accept()
+            self.client = new_client
+        
+        return isValid
         #uri = server.recv(1024).decode()
         #self.network = Pyro4.Proxy(uri)
         #server.close()
+    
+    
+    
+    def send(self,command):
+        self.client.send(command.encode('ascii'))
         
-    def action(self,command):
-        self.server.send(command.encode('ascii'))
-        return self.server.recv(1024).decode('ascii')
+    def recv(self):
+        return self.client.recv(1024).decode('ascii')
+
+
+class StreamRequestMininet(SocketServer.StreamRequestHandler):
+    def handle(self):
+        logger = self.connection.recv(1024)
+        msg = logger.decode('ascii')
+        print msg
+
+class ClientMininet(threading.Thread):
+    
+    _shutdown = False
+    
+    def __init__(self):
+        self.sock = socket(AF_INET, SOCK_STREAM)
+    
+    def connect_server(self,host):
+        isValid = self.validate_ip(host)
+        if isValid:
+            try:
+                self.sock.connect((host,DEFAULT_TCP_LOGGING_PORT))
+                isValid = True
+            except socket.error:
+                pass
+        return valid
+    
+    def validate_ip(self,host):
+        valid = False
+        if isinstance(host,str):
+            if not host or host == 'localhost':
+                valid = True
+            else:
+                try:
+                    valid = bool(socket.inet_aton(host))
+                except socket.error:
+                    pass
+        return valid
+    
+    def run(self):
+        self.
+            
+        
         
 if __name__ == '__main__':
     network = RemoteNetwork()
@@ -38,9 +89,10 @@ if __name__ == '__main__':
     print('Conexao estabelecida')
     options = ''
     
-    while options != 'exit':
-        options = raw_input('> ')
-        print(network.action(options))
+    options = raw_input('> ')
+    network.send(options)
+    print network.recv()
+    
     network.server.close()
     print('Fim da aplicacao')
     
